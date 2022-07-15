4394640c72a56519b2177d6481d37cd473ef8add
Youness AHASLA <48359259+UnesseAh@users.noreply.github.com>
2022-07-15 21:49:38 +0100

    Add files via upload

---
+++ 8kyu/16-Reversed Strings/main.js
@@ -0,0 +1,12 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+//answer
+
+function solution(str){
+  return str.split("").reverse().join("");
+}
